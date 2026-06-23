196055cff7b7b05618292c3ffe95fa76ad80f6a4
Gustavo do Rosário Nunes <gustavorosarionunes21@gmail.com>
2026-06-23 13:43:40 -0300

    commit

---
+++ Config/database.php
@@ -0,0 +1,26 @@
+<?php 
+
+$host = '127.0.0.1';
+$porta = '3307';
+$banco = 'atende_lab';
+$usuario = 'root';
+$senha = '';
+
+try {
+	$pdo = new PDO("mysql:host={$host};port={$porta};dbname={$banco};charset=utf8mb4",
+		$usuario,
+		$senha
+	);
+
+	$pdo->setAttribute(
+		PDO::ATTR_ERRMODE,
+		PDO::ERRMODE_EXCEPTION
+	);
+
+	$pdo->setAttribute(
+		PDO::ATTR_DEFAULT_FETCH_MODE,
+		PDO::FETCH_ASSOC
+	);
+} catch(PDOEXCEPTION $e) {
+	exit('Erro ao conectar com o banco de dados.');
+}
